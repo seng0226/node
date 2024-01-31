1f92d111e64c3266293bf4f0a947a95f97d7dec6
seng0226 <118881380+seng0226@users.noreply.github.com>
2024-01-31 16:16:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const path = require('path');
 const helmet = require("helmet")
 const port = 8060;
+
 // Use the whole root as static files to be able to serve the html file and
 // the build folder
 app.use(express.static(path.join(__dirname, '/')));
