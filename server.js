29d3cf4854e90d8ac2bfb9d7ed07b8074d058e4d
seng0226 <118881380+seng0226@users.noreply.github.com>
2024-01-31 16:02:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const cors = require('cors')
 const path = require('path');
+const helmet = require("helmet")
 const port = 8060;
 // Use the whole root as static files to be able to serve the html file and
 // the build folder
@@ -10,6 +12,13 @@ app.use(express.static(path.join(__dirname, '/')));
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, + '/index.html'));
 })
+app.use(
+    helmet({
+      xFrameOptions: { action: "deny" },
+    })
+);
+
+app.use(cors());
 // Create the server and listen on port
 http.createServer(app).listen(port, () => {
 console.log(`Server running on localhost:${port}`);
